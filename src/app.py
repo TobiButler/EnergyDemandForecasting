1df9e527587e313c6098c763a16b032834e42153
TobiButler <tobibutler1@gmail.com>
2024-03-02 14:09:46 -0700

    trying to deploy using dashtools

--- src/app.py
+++ src/app.py
@@ -380,4 +380,6 @@ def display_page(pathname):
 # Run the application when script is run
 if __name__ == '__main__':
     # app.run_server(debug=True, host='0.0.0.0', port=8050)
-    app.run_server(debug=True, host='localhost', port=8050)
+    # app.run_server(debug=True, host='localhost', port=8050)
+    # app.run_server(debug=True, host='localhost')
+    app.run_server(debug=False, host='127.0.0.1', port="8000")
